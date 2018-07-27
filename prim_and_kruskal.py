65621a3a61933a4dca6190d463dff8ddaba01e21
lumenghe <lumenghe@gmail.com>
2018-07-27 12:00:11 +0100

    adding 'create_edges' function

--- prim_and_kruskal.py
+++ prim_and_kruskal.py
@@ -24,3 +24,17 @@ class MatrixGraph:
      #   cost should be the first item in heapqueue because queue is sorted by the first item
             heapq.heappush(heapqueue, (cost, root, neighbor))
         return heapqueue
+
+    def create_edges(self):
+        self.colors = {}
+        edges = []
+        self.tree_edges = []
+        self.total_cost = 0
+        for node in xrange(len(self.matrix)):
+            self.colors[node] = node
+
+        for (node_1, node_2), cost in numpy.ndenumerate(self.matrix):
+            if node_1 < node_2 and cost:
+                edges.append((node_1, node_2, cost))
+        edges = sorted(edges, key=itemgetter(2))
+        return edges
