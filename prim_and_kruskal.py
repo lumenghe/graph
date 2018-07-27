552d4e69120290c7daa866fe76f3b86726826d4b
lumenghe <lumenghe@gmail.com>
2018-07-27 12:00:00 +0100

    adding 'create_priority_queue'

--- prim_and_kruskal.py
+++ prim_and_kruskal.py
@@ -7,3 +7,10 @@ class MatrixGraph:
     def __init__(self, matrix):
         self.matrix = matrix
         self.tree_edges = []
+
+    def create_priority_queue(self, root):
+        self.tree = []
+        priority_queue = []
+        priority_queue = self.update_priority_queue(priority_queue, root, None)
+        self.total_cost = 0
+        return priority_queue
