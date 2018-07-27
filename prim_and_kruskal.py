99e7f9bc6bfa737ade287686de97580c7546946b
lumenghe <lumenghe@gmail.com>
2018-07-27 12:00:05 +0100

    add function 'create_heapqueue'

--- prim_and_kruskal.py
+++ prim_and_kruskal.py
@@ -14,3 +14,13 @@ class MatrixGraph:
         priority_queue = self.update_priority_queue(priority_queue, root, None)
         self.total_cost = 0
         return priority_queue
+
+    def create_heapqueue(self, root):
+        heapqueue = []
+        self.total_cost = 0
+        self.tree = [root]
+        self.tree_edges = []
+        for neighbor, cost in self.get_neighbors(root):
+     #   cost should be the first item in heapqueue because queue is sorted by the first item
+            heapq.heappush(heapqueue, (cost, root, neighbor))
+        return heapqueue
