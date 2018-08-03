e765089fd560399553af4138aa5616419af8c232
lumenghe <lumenghe@gmail.com>
2018-08-03 14:15:52 +0100

    adding function 'reset_color_and_distance'

--- graph_breadth_first_search.py
+++ graph_breadth_first_search.py
@@ -6,3 +6,7 @@ class MatrixGraph:
     def __init__(self, matrix):
         self.matrix = matrix
         self.reset_color_and_distance()
+
+    def reset_color_and_distance(self):
+        self.color = ["white" for node in range(len(self.matrix))]
+        self.distance = [None for node in range(len(self.matrix))]
